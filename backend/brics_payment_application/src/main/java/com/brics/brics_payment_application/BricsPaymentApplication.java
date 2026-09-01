27025859cb9d454a18c0bc16cba0b83e63ee6c41
João <devnogg.yamamoto@gmail.com>
2026-09-01 17:31:32 -0300

    create basis of project

---
+++ backend/brics_payment_application/src/main/java/com/brics/brics_payment_application/BricsPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.brics.brics_payment_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BricsPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BricsPaymentApplication.class, args);
+	}
+
+}
---
+++ backend/brics_payment_application/src/test/java/com/brics/brics_payment_application/BricsPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brics.brics_payment_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BricsPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
